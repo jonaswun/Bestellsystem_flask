bb404bb2f5c0947ab72aef8901d1a0485f29c1d6
Jonas Wunderle <wunderle11@gmail.com>
2025-04-28 22:23:12 +0200

    Add Printer Handle

---
+++ Printer.py
@@ -0,0 +1,48 @@
+"""Python Implementation for the Printer Setup"""
+
+from escpos.printer import Network
+
+class Printer:
+    """
+    Printer Class which enables printing of Order Data on a EPSON order printer
+    """
+
+    def __init__(self, ip_address:str, logo_path:str=None) -> None:
+        self.printer_handle = Network(ip_address, profile='TM-T20II')
+        assert self.printer_handle.is_online()
+        self.logo_path = logo_path
+
+    def print_logo(self, image_path:str) -> None:
+        """
+        Prints the given logo
+        Use a .png format (others are possible to)
+        Recommended image height is around 250px
+
+        Parameters
+            image_path:str      Path of the Image
+        """
+        self.printer_handle.image(image_path, impl='graphics', center=True)
+
+    def print_items(self, items):
+        self.printer_handle.set(align='left', bold=False)
+        self.printer_handle.textln("")
+        total_order_price = 0
+        for item in items:
+            total_item_price = item['price'] * item['quantity']
+            total_order_price = total_order_price + total_item_price
+            self.printer_handle.textln("{:<20} {:>7.2f}€".format(item['name'], total_item_price))
+            if item['quantity'] > 1:
+                self.printer_handle.textln("{:>10}x {:>7.2f}€".format(item['quantity'], item['price']))
+
+        self.printer_handle.set(align='left', bold=True, double_height=True)
+        self.printer_handle.textln(f'Gesamt: {total_order_price:<20}')
+        self.printer_handle.set(align='left', bold=False)
+
+    def print_order(self, items):
+        if self.logo_path is not None:
+            self.print_logo(self.logo_path)
+        self.print_items(items)
+        self.printer_handle.cut()
+
+    def __del__(self) -> None:
+        pass
--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+import json
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-import json
+from Printer import Printer
 
 app = Flask(__name__)
 CORS(app)
+
+printer = None
+
+printer = Printer("192.168.0.24", logo_path='Rucksackberger_solo.png')
 
 # Load menu from JSON file
 with open("menu.json") as f:
@@ -17,7 +22,14 @@ def get_menu():
 @app.route("/order", methods=["POST"])
 def place_order():
     data = request.json
+    printer.print_order(data['orderedItems'])
+
     return jsonify({"message": "Order received!", "order": data})
 
+def main():
+    print("hallo")
+
+    app.run(debug=False)
+    
 if __name__ == "__main__":
-    app.run(debug=True)
+    main()
